635fb995df3099dcf2e34f3baf22df0b0b2eafc3
Diego-Sabino <dbrgamei5@gmail.com>
2022-11-18 15:43:35 -0300

    req 05

--- src/Archetypes/Archetype.ts
+++ src/Archetypes/Archetype.ts
@@ -23,7 +23,7 @@ export default abstract class Archetype {
     return this._cost;
   }
 
-  static createdRacesInstances(): number {
+  static createdArchetypeInstances(): number {
     throw new Error('Not implemented');
   }
 
